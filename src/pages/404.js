d64536d26bf152bc4e4040adfc9114fd6a07fb66
Erwin Amador <eamador0305@gmail.com>
2025-11-11 19:22:28 -0600

    Create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Noencontrado() {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Lo sentimos, esta página no existe</p>
+    </div>
+  )
+}
