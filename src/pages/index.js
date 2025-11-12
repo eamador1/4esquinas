69abd6fe8536da5ad9501adc6f9d17163fbabb49
Erwin Amador <eamador0305@gmail.com>
2025-11-11 19:19:17 -0600

    Create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import * as React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <section>
+    <Navbar />
+  </section>
+  )
 }
