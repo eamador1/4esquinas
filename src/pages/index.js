2e3d8106f282fa3dc17c0cd050e1a891dabbfdc1
Erwin Amador <eamador0305@gmail.com>
2025-11-12 21:13:41 -0600

    add hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from "../components/Layout"
+import hero from '../assets/vista frente.jpg';
 
 export default function Home() {
   return (
     <Layout>
       <section>
+        <img className="hero" src={hero} alt="Hero"/>
    
       </section>
     </Layout>
