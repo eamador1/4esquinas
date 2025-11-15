c23fa9bf1dea56a40aabc5e0a4c599951d8c2d1a
Erwin Amador <eamador0305@gmail.com>
2025-11-14 20:36:17 -0600

    Add plugins for static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,4 +9,23 @@
  */
 module.exports = {
   plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+  ],
 }
+
+
