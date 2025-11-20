238ac166d11eb97dda80f72bc0a05f6ae7c5eefb
Erwin Amador <eamador0305@gmail.com>
2025-11-19 20:21:04 -0600

    Wrap principios-details.js to show as modal

---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import * as styles from '../Styles/principios-details.module.css'
+
+
+export default function Modal({ slug, contentHtml, title, featuredImg, onClose }) {
+  return (
+    <div className={styles.modalOverlay} 
+    onClick={onClose} 
+    role="button" 
+    tabIndex={0}
+    onKeyDown={(e) => e.key === "Enter" && onClose()}
+    >
+      <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
+        <button onClick={onClose}>CERRAR</button>
+        {featuredImg && <GatsbyImage image={getImage(featuredImg)} alt={title} />}
+        <h2>{title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      
+    </div>
+    </div>
+  );
+}
