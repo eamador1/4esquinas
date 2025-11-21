51bb7b3d3933a6a3981088ae3ad53e4d317363ca
Erwin Amador <eamador0305@gmail.com>
2025-11-20 20:57:26 -0600

    Add video component

---
+++ src/components/LatestVideo.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Video({ videoSrcURL, videoTitle }) {
+  const embedURL = `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(
+    videoSrcURL
+  )}&show_text=false`;
+
+  return (
+    <div className="video">
+      <iframe
+        title={videoTitle}
+        src={embedURL}
+        width="500"
+        height="700"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+}
+
