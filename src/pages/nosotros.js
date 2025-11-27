bbfeed06104bba6191116d7dd530728959493036
Erwin Amador <eamador0305@gmail.com>
2025-11-27 16:24:34 -0600

    Add lorem ipsum content to Nosotros page

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
+import * as styles from '../Styles/nosotros.module.css';
 
 export default function Nosotros() {
     return(
         <Layout>
         <div>
-          <h1>NOSOTROS</h1>
+          <h2>¿Quienes somos?</h2>
+          <div className={styles.somosCont}>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit eu libero risus erat vehicula, nam non nascetur fermentum placerat nec mi ac cursus est vitae turpis praesent, et scelerisque aliquam ullamcorper penatibus vulputate condimentum nullam lacinia vivamus malesuada. Sem sagittis rutrum tincidunt curae conubia, malesuada mus bibendum urna magnis dui, eros quam imperdiet dignissim. Tortor ridiculus vulputate posuere aenean pharetra fusce commodo tempor ullamcorper, cum luctus elementum dapibus phasellus eget blandit risus sed, praesent urna hac suspendisse lectus habitasse facilisi eleifend. Dictum himenaeos eros facilisis sed placerat neque taciti phasellus nibh fusce aliquam, consequat hac vitae porttitor tincidunt lobortis diam platea cum rhoncus lacus vel, sociosqu augue cras suscipit quam ultrices dictumst non fames lectus. Egestas nisi rhoncus aliquam a posuere vitae eget donec montes massa cum congue, eu lectus hendrerit eros primis morbi iaculis ad vestibulum consequat.</p>
+
+<p>Eu posuere morbi auctor vestibulum sociosqu sed bibendum rhoncus pulvinar sodales varius molestie, cursus facilisis hac fusce laoreet fringilla congue convallis arcu hendrerit. Sem nunc natoque aliquam curabitur interdum lectus vehicula tellus, odio laoreet rutrum netus penatibus class scelerisque dapibus, montes semper etiam ultricies torquent fusce congue. Odio lobortis quam aliquet mi nascetur sociosqu neque tincidunt conubia penatibus praesent, duis id quisque accumsan leo venenatis diam litora felis dignissim, vehicula facilisi habitant lacinia eros porta dictumst malesuada nostra tristique. Mauris parturient lacinia risus porttitor velit urna aliquam penatibus odio mattis fringilla fusce, vitae phasellus nunc felis dapibus sodales at suscipit ad in auctor sagittis, taciti non cum erat cras potenti libero semper nulla eros orci. </p>
+
+<p>Aenean himenaeos ligula molestie euismod vitae cras id in libero tristique posuere, cubilia sagittis erat etiam nec facilisis pellentesque tortor placerat. Etiam quisque nec placerat ad vehicula cursus, rhoncus tempus primis ultricies eget dui diam, luctus euismod nulla massa auctor. </p>
+
+<p>Ultricies eu tempor blandit tincidunt per ornare penatibus laoreet, fusce venenatis elementum auctor platea tempus. Netus leo eleifend et consequat laoreet montes nascetur eget, rutrum fringilla platea risus mus commodo nunc, cursus vel ligula facilisi dictum habitant ornare. Magnis id cubilia a ligula netus nec nisi per fames, euismod tortor quis varius blandit eros nulla maecenas urna lacus, risus velit aptent gravida massa porta ad quisque. Cum curae massa nulla posuere mauris hendrerit velit, sed potenti aliquet cubilia in pulvinar ligula ullamcorper, dapibus taciti porttitor consequat nisi aliquam. </p>
+
+<p>Imperdiet hendrerit non lacus cursus potenti curae hac eleifend laoreet montes aliquet, senectus suscipit diam cras pellentesque tortor netus augue primis. Egestas velit gravida tortor mauris praesent lectus posuere eget, inceptos integer natoque quisque rhoncus urna semper tempus eros, ultricies sagittis fusce malesuada platea cursus justo. Accumsan nunc lacinia primis nullam id quam bibendum mollis urna et natoque lobortis mattis volutpat, ante augue dignissim magnis placerat praesent blandit proin quisque gravida curabitur nec penatibus. </p>
+</div>
         </div>
         </Layout>
     )
