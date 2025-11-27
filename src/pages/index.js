7ec84826d7f0c8d5e26829408f470f4d8fa3c189
Erwin Amador <eamador0305@gmail.com>
2025-11-27 16:24:01 -0600

    Add header and descrition to video in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { graphql} from "gatsby";
 import Layout from "../components/Layout"
 import * as styles from '../Styles/index.module.css';
 import { StaticImage } from "gatsby-plugin-image";
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import Modal from "../components/Modal"
-import Video from "../components/LatestVideo"
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Modal from "../components/Modal";
+import Video from "../components/LatestVideo";
 
 
 export default function Home({ data }) {
@@ -68,16 +68,37 @@ export default function Home({ data }) {
 
        
   
-    <div>
-      <h1>Prédica Reciente</h1>
+    <div className={styles.videoArea}>
+      <h3 className={styles.tituloPredica}>Caracter humilde</h3>
+      <p className={styles.fechaPredica}>23 de Noviembre 2025</p>
+      <p className={styles.descripcionPredica}>
+        El pastor Jonathan Soto explica como la humildad es una característica esencial en un cristiano.
+      </p>
         <div className={styles.videoCont}>
           <Video
-            videoSrcURL="https://www.facebook.com/reel/1833170947592003"
-            videoTitle="El caracter de un Critiano Parte I"
+            videoSrcURL="https://www.facebook.com/somos4troesquinas/videos/24968298472853948" 
           />
         </div>
       </div>
 
+      </section>
+      <section className={styles.serviciosSection}>
+        <h3>Nuestros Servicios</h3>
+        <p>Únete a nosotros a adorar a nuetro Señor Jesucristo durante la semana</p>
+        <div className={styles.serviciosWrapper}>
+        <div>CULTO DE ORACIÓN
+        <div className={styles.libroCont}>
+          <StaticImage
+            className={styles.libro}
+            src="../assets/libro-alt.svg"
+            alt="libro"
+          />
+        </div>
+        </div>
+        <div>CULTO DE FRATERNIDADES</div>
+        <div>ESCUELA BÍBLICA</div>
+        <div>CULTO EVANGELÍSTICO</div>
+        </div>
       </section>
     </Layout>
   );
